dce65c2c746b056331e3913ecd7bb7bbf4b572c8
websummary <Morgenholt86@gmail.com>
2022-01-07 18:05:21 +0500

    refactor: update interpolation and delete excess

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 module.exports = function toReadable (n) {
-  const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
   const dozens = [' ', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   
-  if (n == 0) { 
-    return 'zero'; 
-
-  } if (n < 20) {
+   if (n < 20) {
     return units[n];
-
-  } if (n == 10){
-    return dozens[1];
 
   } if (n < 100) {
     return `${dozens[n.toString()[0]]}${n.toString()[1] > 0 ? ' ' + units[n.toString()[1]] : ''}`;
@@ -19,6 +13,6 @@ module.exports = function toReadable (n) {
 
   } 
   if (100 > n < 1000) {
-    return units[n.toString()[0]] + ' hundred' + `${n % 100 ? ' ' + toReadable(n % 100) : '' }`;
+    return `${units[n.toString()[0]]} hundred${n % 100 ? ' ' + toReadable(n % 100) : '' }`;
   } 
 }
