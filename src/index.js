9f338e48665208d584084a642fd71547c0fb1c89
websummary <Morgenholt86@gmail.com>
2022-01-09 15:30:44 +0500

    refactor: delete excess

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 module.exports = function toReadable (n) {
   const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-  const dozens = [' ', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const dozens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   
    if (n < 20) {
-    return units[n];
+     return units[n];}
 
-  } if (n < 100) {
-    return `${dozens[n.toString()[0]]}${n.toString()[1] > 0 ? ' ' + units[n.toString()[1]] : ''}`;
+   if (n < 100) {
+     return `${dozens[String(n)[0]]}${String(n)[1] > 0 ? ' ' + units[String(n)[1]] : ''}`;}
 
-  } if (n == 100) {
-    return 'one hundred';
-
-  } 
-  if (100 > n < 1000) {
-    return `${units[n.toString()[0]]} hundred${n % 100 ? ' ' + toReadable(n % 100) : '' }`;
-  } 
+   if (n < 1000) {
+     return `${units[String(n)[0]]} hundred${n % 100 ? ' ' + toReadable(n % 100) : '' }`;} 
 }
